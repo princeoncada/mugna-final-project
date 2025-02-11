d26e90f2d97a232af6ce94cef7ec22ce4500adcc
prince <prince.oncada@gmail.com>
2025-02-11 05:38:29 -0500

    dev: created api routes for todos requests

---
+++ app/api/todos/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import axios from "axios";
+
+export async function GET(req: NextRequest) {
+    const access = req.headers.get("Authorization");
+    const API_URL = process.env.API_URL + "/todos/"; // Use hidden API URL
+    const response = await axios.get(API_URL, {
+        headers: { Authorization: access },
+    });
+    return NextResponse.json(response.data);
+}
+
+export async function POST(req: NextRequest) {
+    const access = req.headers.get("Authorization");
+    const API_URL = process.env.API_URL + "/todos/"; // Use hidden API URL
+    const data = await req.json();
+    const response = await axios.post(API_URL, data, {
+        headers: { Authorization: access },
+    });
+    return NextResponse.json(response.data);
+}
