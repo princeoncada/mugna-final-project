8e178d49222f558906f9e7fd33c93ba5849b7ae3
prince <prince.oncada@gmail.com>
2025-02-12 04:34:22 -0500

    dev: fixed it middleware to allow getting of access token with cloudflare pages, also included a new redirect function that uses a callback function to logout via AuthContext

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,29 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { reauthenticate } from "./lib/services/authService";
+import { triggerLogout } from "./lib/authhandler";
+
+function redirect(url: string) {
+    triggerLogout();
+    return NextResponse.redirect(url);
+}
 
 export function middleware(req: NextRequest) {
-    const access_token = req.cookies.get("access_token")?.value;
-    const refresh_token = req.cookies.get("refresh_token")?.value;
+    const cookies = req.headers.get("cookie") ?? "";
+    const accessTokenMatch = /access_token=([^;]*)/.exec(cookies);
+    const access_token = accessTokenMatch ? accessTokenMatch[1] : null;
+    const refreshTokenMatch = /refresh_token=([^;]*)/.exec(cookies);
+    const refresh_token = refreshTokenMatch ? refreshTokenMatch[1] : null;
     const isLoginPage = req.nextUrl.pathname === "/login";
     const baseUrl = req.nextUrl.origin;
 
-    // Handle refresh token logic
-    if (!access_token && refresh_token) {
-        // Refresh token logic here
-        reauthenticate().then((success) => {
-            if (!success) {
-                return NextResponse.redirect(`${baseUrl}/login`);
-            }
-        });
-    }
-
-    // Redirect to login page if no access token and not on login page
+    // Redirect to login if no access token and not on login page
     if (!access_token && !refresh_token && !isLoginPage) {
-        return NextResponse.redirect(`${baseUrl}/login`);
+        return redirect(`${baseUrl}/login`);
     }
 
     // Redirect to home page if access token and on login page
     if (access_token && isLoginPage) {
-        return NextResponse.redirect(`${baseUrl}/`);
+        return redirect(`${baseUrl}/`)
     }
 
     return NextResponse.next();
@@ -34,4 +32,4 @@ export function middleware(req: NextRequest) {
 export const config = {
     // Only run middleware for these paths
     matcher: ["/login", "/", "/todos/:path*"],
-}
+};
